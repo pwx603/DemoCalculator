63211e2410d7a732595d53f76365575d009cb8d3
Wen Xing Pan <t6o0b@ugrad.cs.ubc.ca>
2017-11-15 12:52:44 -0800

    improve by using switch statements

--- src/com/app/democalculator/Main.java
+++ src/com/app/democalculator/Main.java
@@ -3,26 +3,45 @@ package com.app.democalculator;
 public class Main {
 
     public static void main(String[] args) {
-	    double val1 = 100.0d;
-	    double val2 = 0d;
-        double result;
+        double[] leftvals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightvals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'a', 's', 'd', 'm'};
+        double[]results = new double[opCodes.length];
 
-        char opCode = 'd';
+//	    double val1 = 100.0d;
+//	    double val2 = 0d;
+//        double result;
 
-        if(opCode == 'a')
-            result = val1 + val2;
-        else if(opCode == 's')
-            result = val1 - val2;
-        else if(opCode == 'd')
-            result = (val2 != 0.0d)? val1/val2: 0.0d;
-        else if(opCode == 'm')
-            result = val1 * val2;
-        else{
-            System.out.println("Error - invalid opCode");
-            result = 0d;
+//        char opCode = 'd';
+
+        for(int i = 0; i < opCodes.length; i++){
+            switch (opCodes[i]){
+                case 'a':
+                    results[i] = leftvals[i] + rightvals[i];
+                    break;
+                case 's':
+                    results[i] = leftvals[i] - rightvals[i];
+                    break;
+                case 'd':
+                    results[i] = (rightvals[i]!=0.0d)? leftvals[i]/rightvals[i]:0.0d;
+                    break;
+                case 'm':
+                    results[i] = leftvals[i] * rightvals[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0d;
+                    break;
+
+            }
+
         }
 
-        System.out.println(result);
+
+        for(double theResult: results){
+            System.out.println(theResult);
+        }
+
 
     }
 }
