5ddc987233b8fc8a783fc8d57996658bd32d0377
Wen Xing Pan <t6o0b@ugrad.cs.ubc.ca>
2017-11-15 12:37:28 -0800

    Extremely simple calculator

--- src/com/app/democalculator/Main.java
+++ src/com/app/democalculator/Main.java
@@ -3,6 +3,26 @@ package com.app.democalculator;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    double val1 = 100.0d;
+	    double val2 = 0d;
+        double result;
+
+        char opCode = 'd';
+
+        if(opCode == 'a')
+            result = val1 + val2;
+        else if(opCode == 's')
+            result = val1 - val2;
+        else if(opCode == 'd')
+            result = (val2 != 0.0d)? val1/val2: 0.0d;
+        else if(opCode == 'm')
+            result = val1 * val2;
+        else{
+            System.out.println("Error - invalid opCode");
+            result = 0d;
+        }
+
+        System.out.println(result);
+
     }
 }
